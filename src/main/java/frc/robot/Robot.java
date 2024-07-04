d47494259d74bb2acc2fea86783f745652d3b6b3
Ravi Ponmalai <rponmalai@gmail.com>
2024-07-03 18:38:08 -0700

    added a comment

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,6 +2,8 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+//example comment
+
 package frc.robot;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
